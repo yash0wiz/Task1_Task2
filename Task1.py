69f5cd643ea489b7c1222c48f2976e5b9fd3756b
yash0wiz <141996224+yash0wiz@users.noreply.github.com>
2025-05-30 08:52:28 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+num3=num1+num2
+print("Addition",num3)
+num3=num1-num2
+print("Subtraction",num3)
+num3=num1*num2
+print("Multiplication",num3)
+num3=num1/num2
+print("Division",num3)
